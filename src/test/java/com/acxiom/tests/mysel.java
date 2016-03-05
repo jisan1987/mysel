5c7738b162c042b4e65b60fcce1e38cd346d5801
jisan1987 <jisan_85@yahoo.com>
2016-03-04 19:39:02 -0600

    2nd commit

--- src/test/java/com/acxiom/tests/mysel.java
+++ src/test/java/com/acxiom/tests/mysel.java
@@ -1,15 +1,26 @@
 package com.acxiom.tests;
 
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class mysel {
 
+
+public class mysel {
+protected WebDriver dr;
 	
 @Test
-public void hey(){
-WebDriver dr = new FirefoxDriver();
+public void hey() throws IOException{
+DesiredCapabilities caps = DesiredCapabilities.firefox();
+caps.setBrowserName("firefox");
+caps.setPlatform(Platform.WINDOWS);
+dr=new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), caps);
 	dr.get("http://www.google.com");
 	
 }
